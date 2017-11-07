b935a7a39141cc78878b9ed5f22b5c26921337d2
钟青林 <zhongqinglin@ut.cn>
2017-11-07 17:04:12 +0800

    init

---
+++ muke.js
@@ -0,0 +1,31 @@
+function begin () {
+  let list = document.getElementById('chapterList').getElementsByTagName('li')
+  let len = list.length
+
+  // 视频播放完毕，继续播放
+  let video = document.getElementsByClassName('vjs-tech')[0]
+  video.addEventListener('ended', () => {
+    playVideo()
+  })
+
+  // 播放视频
+  function playVideo () {
+    for (let i = 0; i < len; i++) {
+      let watchstate = list[i].getAttribute('watchstate')
+      if (watchstate === '0' || watchstate === '2') {
+        list[i].click(); // 观看视频
+        document.querySelector('.volumeIcon').click(); // 关闭声音
+        document.querySelector('.speedTab15').click(); // 1.5倍加速
+        document.querySelector('.line1bq').click(); // 标清
+        break
+      }
+    }
+  }
+
+  playVideo()
+  console.log('脚本成功运行中...')
+}
+
+setTimeout(() => {
+  begin()
+}, 5000)
