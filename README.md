2470689ee5725d684549bdbe852be5982ce5e4c2
perhaps <perhapszql@gmail.com>
2018-03-31 20:38:32 +0800

    更新完毕

--- README.md
+++ README.md
@@ -7,9 +7,10 @@
 
 后续：
 
-- 这个插件我是在2017年12月初写完的，之后再也没有进行调试，所以如果智慧树官网的前端代码发生变化的话，这个插件就会不起作用的
-- 今天是2018年3月23号，有朋友向我反映插件用不了。因为我自己的课程也修完了，所以会找师弟师妹借账号，让我调试一下插件代码，我需要点时间，不好意思。还有，因为我现在大四，忙着毕业设计，可能比较少精力放这里，有问题的话请邮件跟我说：perhapszql@gmail.com。
-- 今天是2018年3月31号，没能借到智慧树账号，代码无法调试，所以这个插件暂时不能用。请各位还是老老实实的上网课吧~不好意思了。
+- 2017年12月初，插件写完。
+- 今天是2018年3月31号，代码第一次更新完毕。添加了视频教程。
+
+下面是教程。
 
 ## 一、功能和工具
 
@@ -20,33 +21,47 @@
 - 自动播放视频的下一集
 - 对偶尔弹出的`测试`对话款进行关闭处理
 
+未实现的功能：
+
+- 单元测试(这个还是自己做的！)
+
 需要的工具：
 
-- chrome55+ (我用了es7的语法: `async/await` , 只有chrome55+的js引擎才能解析)
+- chrome浏览器
 
-## 二、使用方法
+## 二、图文教程
 
-1、打开chrome浏览器，来到 [github.com/perhaps-yo/zhihuishu](github.com/perhaps-yo/zhihuishu) 界面，下载插件源码
+1、下载最新版chrome浏览器，下载地址：[https://www.google.cn/chrome/](https://www.google.cn/chrome/)
+
+![下载chrome](./images/download-chrome.png)
+
+2、打开chrome浏览器，来到 [github.com/perhaps-yo/zhihuishu](github.com/perhaps-yo/zhihuishu) 界面，也就是当前页面，下载插件源码
 
 ![下载插件](./images/download.png)
 
 解压后的源码放到自己喜欢的目录
 
-2、chrome浏览器输入 `chrome://extensins` ，来到扩展程序页面。按步骤：勾选`开发者模式` -> 单击`加载已解压的扩展程序`，选择刚刚源码的位置 -> 勾选`已启用`。效果如下图：
+3、chrome浏览器输入 `chrome://extensins` ，来到扩展程序页面。按步骤：勾选`开发者模式` -> 单击`加载已解压的扩展程序`，选择刚刚源码的位置 -> 勾选`已启用`。效果如下图：
 
 ![安装插件](./images/add.png)
 
 脚本安装成功，准备刷课！
 
-3、来到智慧树视频网站 [http://online.zhihuishu.com/onlineSchool/student/index](http://online.zhihuishu.com/onlineSchool/student/index)，登录后，到需要刷课的视频页面，就会自动开始刷课啦～
+4、来到智慧树视频网站 [http://online.zhihuishu.com/onlineSchool/student/index](http://online.zhihuishu.com/onlineSchool/student/index)，登录后，到需要刷课的视频页面，就会自动开始刷课啦～
 
 ![自动刷课](./images/play.png)
 
-## 三、遇见的问题
+## 三、视频教程
+
+正在录制中。。
+
+## 四、遇见的问题
 
 - Q: 来到智慧树视频网站后，没能实现 `1.5倍速度，标清，无声播放`？
 - A: 按 `f5` 刷新页面。
+- Q: 还是不行的话？
+- A: email联系我吧：perhapszql@gmail.com。可是我不一定能解决问题。
 
-## 四、License
+## 五、License
 
 [MIT](https://github.com/aspnetboilerplate/aspnetboilerplate/blob/dev/LICENSE).
--- zhihuishu.js
+++ zhihuishu.js
@@ -5,22 +5,18 @@
  * @description 智慧树视频自动播放下一集，并以1.5倍速度，标清，无声播放
  */
 
-let list = null // 整个视频播放列表
-let video = null // video元素
-let close = null // 偶尔弹出的对话框的关闭按钮
-
 /**
  * dom 加载完毕才开始执行脚本
  */
-window.onload = function() {
-  setTimeout(() => begin(), 3000)
+window.onload = function () {
+  begin()
 }
 
 async function begin() {
-  list = getElement('list')
-  video = getElement('video')
-  await playVideo() // 获取video后，按要求播放视频
-  await background()
+  let list = getElement('list') // 整个视频播放列表
+  let video = getElement('video') // video元素
+  await playVideo(list, video) // 获取video后，按要求播放视频
+  background() // 隔10s就检查视频是否播放完毕，是否弹出'测试'对话框
   console.log('脚本成功运行中...')
 }
 
@@ -28,27 +24,47 @@ async function begin() {
  * 播放视频
  * @returns void
  */
-async function playVideo() {
-  for (let i = 0, len = list.length; i < len; i++) {
-    let watchstate = list[i].getAttribute('watchstate')
-    let id = list[i].getAttribute('id')
-    // 视频没被播放过并且不是标题行
-    if ((watchstate === '0' || watchstate === '2') && id !== 'video-0') {
-      list[i].click() // 播放视频
-      setTimeout(() => specialEffect(), 3000) // 按要求播放视频
-      break
+async function playVideo(list, video) {
+  try {
+    for (let i = 0, len = list.length; i < len; i++) {
+      let watchstate = list[i].getAttribute('watchstate')
+      let id = list[i].getAttribute('id')
+      // 视频没被播放过并且不是标题行
+      if ((watchstate === '0' || watchstate === '2') && id !== 'video-0') {
+        list[i].click() // 播放视频
+        return new Promise((resolve) => { // 3s后按要求播放视频
+          setTimeout(() => {
+            specialEffect(video)
+            resolve()
+          }, 3000)
+        })
+      }
     }
+  } catch (error) {
+    console.log(error)
   }
+}
+
+/**
+ * 按要求播放视频: 1.5倍速度，标清，无声播放
+ * @returns void
+ */
+function specialEffect(video) {
+  video.currentTime = 2 // 视频重新播放
+  if (video.paused && typeof video.play === 'function') video.play() // 视频停止的话，继续播放
+  getElement('volumn').click() // 关闭声音
+  getElement('speedTab').click() // 1.5倍加速
+  getElement('sharpness').click() // 标清
 }
 
 /**
  * 隔10s就检查视频是否播放完毕，是否弹出'测试'对话框
  * @returns void
  */
-async function background() {
+function background() {
   // 每10s检查视频是否播放完毕，是的话，刷新页面
   setInterval(() => {
-    video = getElement('video')
+    let video = getElement('video') // 需要不断的获取video元素
     if (video.ended) {
       console.log('正在刷新页面...')
       window.location.reload()
@@ -56,23 +72,10 @@ async function background() {
   }, 10000)
   // 每10s检查是否弹出'测试'对话框，并关闭
   setInterval(() => {
-    close = getElement('close')
-    if(close !== null) close.click()
+    let close = getElement('close') // 偶尔弹出的对话框的关闭按钮
+    if (close !== null) close.click()
   }, 10000)
 }
-
-/**
- * 按要求播放视频: 1.5倍速度，标清，无声播放
- * @returns void
- */
-function specialEffect() {
-  video.currentTime = 2 // 视频重新播放
-  if (video.paused && typeof video.play === 'function') video.play() // 视频停止的话，继续播放
-  getElement('volumn').click() // 关闭声音
-  getElement('speedTab').click() // 1.5倍加速
-  // getElement('sharpness').click() // 标清
-}
-
 
 /**
  * 获取指定元素
@@ -81,32 +84,39 @@ function specialEffect() {
  */
 function getElement(ele) {
   switch (ele) {
-    case 'list': {
-      list = document.getElementById('chapterList').getElementsByTagName('li')
-      if (list === null) window.alert('网速太慢啦，无法视频播放列表，请刷新页面')
-      return list
-    }
-    case 'video': {
-      video = document.querySelector('.vjs-tech')
-      if (video === null) window.alert('网速太慢啦，无法获取video元素，请刷新页面')
-      return video
-    }
-    case 'volumn': { // 视频的声音控件
-      let volumn = document.querySelector('.volumeIcon')
-      if (volumn === null) window.alert('网速太慢啦，无法获取功能控件，请刷新页面')
-      return volumn
-    }
-    case 'speedTab': { // 视频的速度控件
-      let speedTab = document.querySelector('.speedTab15')
-      return speedTab
-    }
-    case 'sharpness': { // 视频的清晰度控件
-      let sharpness = document.querySelector('.line1bq')
-      return sharpness
-    }
-    case 'close': { // '测试'对话框关闭按钮
-      let close = document.querySelector('.popboxes_close')
-      return close
-    }
+    case 'list':
+      {
+        let list = document.getElementById('chapterList').getElementsByTagName('li')
+        if (list === null) window.alert('网速太慢啦，无法视频播放列表，请刷新页面')
+        return list
+      }
+    case 'video':
+      {
+        let video = document.querySelector('.vjs-tech')
+        if (video === null) window.alert('网速太慢啦，无法获取video元素，请刷新页面')
+        return video
+      }
+    case 'volumn':
+      { // 视频的声音控件
+        let volumn = document.querySelector('.volumeIcon')
+        if (volumn === null) window.alert('网速太慢啦，无法获取功能控件，请刷新页面')
+        return volumn
+      }
+    case 'speedTab':
+      { // 视频的速度控件
+        let speedTab = document.querySelector('.speedTab15')
+        return speedTab
+      }
+    case 'sharpness':
+      { // 视频的清晰度控件
+        let sharpness = document.querySelector('.line1bq')
+        return sharpness
+      }
+    case 'close':
+      { // '测试'对话框关闭按钮
+        let close = document.querySelector('.popboxes_close')
+        // let close = document.querySelector('.popbtn_cancel')
+        return close
+      }
   }
 }
